c6e17e1c56283778ba7e4318906c4d09b7914b5f
ProtoByter <admin@protobyte.tech>
2021-06-28 17:41:37 +0100

    Fixed the /fashbash command

--- fashbash.py
+++ fashbash.py
@@ -1,4 +1,8 @@
+import logging
 import os
+import tabulate
+import git
+from git import *
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
@@ -8,11 +12,13 @@ import pyodbc
 import datetime
 import subprocess
 
+logging.basicConfig(level=logging.INFO)
 load_dotenv()
 
-bot = commands.Bot(command_prefix=".",description="")
-slash = SlashCommand(bot)
+bot = commands.Bot(command_prefix=".", description="")
+slash = SlashCommand(bot, sync_commands=True)
 db: pyodbc.Connection = pyodbc.connect(os.environ["SQL_RW_STRING"])
+
 
 async def checkBan(id) -> list:
     cursor: pyodbc.Cursor = db.cursor()
@@ -64,9 +70,9 @@ async def on_command_error(ctx, error):
                       required=True)
     ])
 async def report(ctx, arg1, oargs):
-    if discord.abc.GuildChannel.permissions_for(ctx.channel,ctx.author).administrator:
+    if discord.abc.GuildChannel.permissions_for(ctx.channel, ctx.author).administrator:
         await addBan(arg1.id, oargs, ctx.guild.id)
-        await ctx.send("Reported "+arg1.name)
+        await ctx.send("Reported " + arg1.name)
     else:
         await ctx.send("You aren't allowed to do that")
 
@@ -87,19 +93,44 @@ async def check(ctx, args):
         i = 0
         for ban in output:
             banned_on = await commands.GuildConverter().convert(ctx, str(ban[2]))
-            output_str += ("**Ban " + str(i+1) + ":**\nServer: " + banned_on.name + "\nExpires: " + str(ban[3].day) + "/" + str(ban[3].month) + "/" + str(ban[3].year) + "\nReason: " + ban[1] + "\n")
+            output_str += ("**Ban " + str(i + 1) + ":**\nServer: " + banned_on.name + "\nExpires: " + str(
+                ban[3].day) + "/" + str(ban[3].month) + "/" + str(ban[3].year) + "\nReason: " + ban[1] + "\n")
             i += 1
         await ctx.send(output_str)
     else:
         await ctx.send("User hasn't been banned previously")
 
+
 @slash.slash(
     name="fashbash",
-    description="Gives some information about this bot"
+    description="Gets some information about FashBash"
 )
-async def fashbash(ctx):
-    output = subprocess.check_output(["git","shortlog","-sne"])
-    await ctx.send("**FashBash**\nFashBash is a small bot which syncs bans between servers designed for the leftist community\nThe GitHub repo is: https://github.com/ProtoByter/FashBash\n**Contributors**\n"+output)
+async def fashbash(ctx: SlashContext):
+    repo = Repo(os.getcwd())
+    o = repo.remotes.origin
+    o.pull()
+    commits = list(repo.iter_commits())
+
+    count = {}
+
+    for commit in commits:
+        try:
+            count[commit.author.name] += 1
+        except KeyError:
+            count[commit.author.name] = 1
+
+    sorted_count = dict(sorted(count.items(), key=lambda item: item[1]))
+
+    sorted_count = [{x[0],str(x[1])} for x in sorted_count.items()]
+
+    output = tabulate.tabulate(sorted_count,["Committer","Commits"],tablefmt="grid")
+    await ctx.send(
+        """**FashBash**
+FashBash is a small bot which syncs bans between servers and is designed for the leftist community to keep fascist ancaps and the like from raiding their server
+The GitHub repo is: https://github.com/ProtoByter/FashBash
+**Contributors**
+```""" + output + "```")
+
 
 @bot.event
 async def on_ready():
